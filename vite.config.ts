ae444c97fb48f3025ed780c3ac63ecc5cba4ccd1
Uzhanin Egor <74734213+Gearonix@users.noreply.github.com>
2023-09-28 20:47:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/optimal-city-test-task',
+  base: '/optimal-city.test-task',
   plugins: [vue()],
   server: {
     port: 3000
